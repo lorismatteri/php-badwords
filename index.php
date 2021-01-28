52364f71950266b49802e365d0d7dbe95eca951a
Loris Matteri <loris.matteri93@gmail.com>
2021-01-28 16:55:30 +0100

    Primo commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $name = "Loris";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Ciao <?php echo $name ?></h1>
+    
+</body>
+</html>
+
