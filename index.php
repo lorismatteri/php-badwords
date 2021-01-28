246aa3ffd352220a1d0126bedb159395f994df79
Loris Matteri <loris.matteri93@gmail.com>
2021-01-28 18:08:31 +0100

    Badword

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     $surname = "Matteri"; */
     $par = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus dolores laborum maxime! Minus, consequatur! Delectus provident suscipit modi voluptatibus in maxime, a odit adipisci deleniti officiis error, incidunt, aut quos?";
     $length = strlen($par);
+    $badword = "ciao";
+    $_GET["badword"];
+    $word = str_replace("ciao", "***", $badword);
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +19,8 @@
 
     <p><?php echo $par;?></p> 
     <p>La lunghezza del paragrafo è di <?php echo $length;?> caratteri</p>
+    
+    <p>La parola inserita è <?php echo $word;?></p>
     
 </body>
 </html>
